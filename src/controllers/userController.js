d376dd13aaf00c203ebd836110b703c23a1ec7bf
dpqls0356 <dpqls0356@gmail.com>
2023-05-01 23:49:08 +0900

    DB에 join 데이터 등록 -> 비번 보안 설정은 안되는 상황

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,18 @@
+import User from "../models/User";
 import { userObj } from "./videoController";
 export const getJoin = (req,res) => {
     return res.render("join",{pageTitle:"join",userObj});
 }
-export const postJoin = (req,res)=>{
-    return res.end();
+export const postJoin = async(req,res)=>{
+    const {name,username,email,password,location} = req.body;
+    await User.create({
+        name,
+        username,
+        email,
+        password,
+        location
+    });
+    return res.redirect("/login");
 }
 export const edit = (req,res) =>{
     res.send("Edit User");

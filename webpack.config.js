0b320eccc45670591d65374a9e77c4fd3e6a8039
dpqls0356 <dpqls0356@gmail.com>
2023-05-18 22:04:57 +0900

    main.js에서 css코드 분리시켜 파일만들기

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
 // entry = 처리해야할 파일위치
 // output = 처리 결과 - filename (처리하여 생성된 파일명) /  path (파일이 저장될 장소, 절대경로가 필요)
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports={
+    plugins: [new MiniCssExtractPlugin({
+        filename:"css/style.css",
+    })],
     entry:"./src/client/js/main.js",
     mode:"development",
     // production
     output:{
-        filename:"main.js",
-        path:path.resolve(__dirname,"assets","js"),
+        filename:"js/main.js",
+        path:path.resolve(__dirname,"assets"),
     },
     module:{
         rules:[
@@ -25,7 +29,7 @@ module.exports={
             {
                 test: /\.scss$/,
                 // webpack는 역순으로 실행되기에 가장 먼저 실행되는 것이 use의 마지막에 들어가야함
-                use: ["style-loader", "css-loader", "sass-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             }
         ]
     }

5bb8879bcb5798da783ef2cc8de54392d0193726
dpqls0356 <dpqls0356@gmail.com>
2023-05-23 18:08:57 +0900

    play 버튼 , mute 버튼 , 음향조절
    const video = document.querySelector("video")를 tagname으로 받아오면 작동안함
    이유 찾아보기...

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,22 +1,55 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
-const muteBtn = document.getElementById("mute");
+const muteBtn = document.getElementById("mute"); 
 const time = document.getElementById("time");
 const volume = document.getElementById("volume");
 
-const handlePlayClick = (e) => {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
-};
-const handlePause = () => (playBtn.innerText = "Play");
-const handlePlay = () => (playBtn.innerText = "Pause");
-
-const handleMute = (e) => {};
-
-playBtn.addEventListener("click", handlePlayClick);
-muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
+const handlePlayClick = (e) =>{
+    if(video.paused){
+        // playBtn.innerHTML="Pause"
+        video.play();
+    }
+    else{
+        // playBtn.innerHTML="Play"
+        video.pause();
+    }
+    playBtn.innerText=video.paused?"Play":"Pause";
+}
+// const handlePause = ()=>{
+//     playBtn.innerHTML="Play";
+// }
+// const handlePlay = ( )=>{
+//     playBtn.innerHTML="Pause";
+// }
+var videoVolume = volume.value;
+video.volume = videoVolume;
+const handleMute = (e)=>{
+    if(video.muted){
+        // 비디오 muted를 false로 바꿔줌
+        video.muted=false;
+        // muteBtn.innerText="OnMute";
+        video.volume = videoVolume;
+        volume.value = videoVolume;
+    }
+    else{
+        video.muted=true;
+        // muteBtn.innerText="UnMute";
+        volume.value=0;
+    }
+    muteBtn.innerText = video.muted?"Unmute":"Mute";
+}
+const handelVolumeChange = (event) =>{
+    videoVolume = event.target.value;
+    video.volume = videoVolume;
+// 뮤트에서 드래그했을때 변경
+    if(video.muted){
+        video.muted =false;
+    }
+    muteBtn.innerText = video.muted?"Unmute":"Mute";
+}
+playBtn.addEventListener("click",handlePlayClick);
+muteBtn.addEventListener("click",handleMute);
+// video.addEventListener("pause",handlePause);
+// video.addEventListener("play",handlePlay);
+volume.addEventListener("input",handelVolumeChange);
+// input - 실시간 값 변화 캐치 가능 change - 최종적인 값만 캐치 가능

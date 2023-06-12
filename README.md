cfc3b3a031a1071584cbbf2c71286fec9919955b
dpqls0356 <dpqls0356@gmail.com>
2023-06-12 17:53:01 +0900

    restart!
    배운 것을 토대로 한번 만들어봅시다..

--- README.md
+++
@@ -1,7 +0,0 @@
-# youtube clone
-
-<br/><br/>
-
-### version1 
-
- - nomader coder의 youtube clone 강의 보면서 따라사 만든 버전
--- src/routers/apiRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {registerView,addComment,deleteComment} from "../controllers/videoController";
-const apiRouter = express.Router();
-
-apiRouter.post("/videos/:id([0-9a-f]{24})/views",registerView);
-apiRouter.post("/videos/:id([0-9a-f]{24})/comment",addComment);
-apiRouter.delete("/videos/:id/delete",deleteComment);
-export default apiRouter;

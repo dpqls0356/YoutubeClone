f512f68487c9f257ceafd0db78ec7938d182816c
dpqls0356 <dpqls0356@gmail.com>
2023-06-21 23:26:02 +0900

    home 수정 + sidebar로 분리해서 생성

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import {getHome} from "../controller/videoController";
-
+import {getJoin} from "../controller/userController";
 const rootRouter = Router();
+
 rootRouter.get("/",getHome);
+rootRouter.route("/join").get(getJoin);
 
 export default rootRouter;

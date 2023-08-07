00957f35db8bc9ce19bf830c4db7de4daa90b9db
dpqls0356 <dpqls0356@gmail.com>
2023-08-07 23:10:04 +0900

    session

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -6,20 +6,21 @@ export const getJoin = (req,res) =>{
 export const postJoin = async(req,res)=>{
     const {username,id,password,checkingpassword,email,birth} =req.body;
     // 아이디가 겹치는지
-    const userExistById = await User.findOne({userid:id});
+    // 아이디 또는 아이디가 겹치는지 알고싶으면 => exists({$of:[{username},{email}]})
+    const userExistById = await User.exists({userid:id});
+    const pageTitle = "Join";
     if(userExistById){
-        console.log(userExistById);
-        return res.render("user/join",{pageTitle:"Join",idErrorMessage:"중복된 아이디입니다..."});
+        return res.status(400).render("user/join",{pageTitle,idErrorMessage:"중복된 아이디입니다..."});
     }
     // 이메일이 겹치는 지 --- 소셜로 가입하는 경우 때문에
     const userExistsByEmail = await User.findOne({email:email});
     if(userExistsByEmail){
         console.log(userExistsByEmail)
-        return res.render("user/join",{pageTitle:"Join",emailErrorMessage:"이미 사용중인 이메일입니다..."});
+        return res.status(400).render("user/join",{pageTitle,emailErrorMessage:"이미 사용중인 이메일입니다..."});
     }
     // 비번이 일치하는지
     if(password!==checkingpassword){
-        return res.render("user/join",{pageTitle:"Join",pwErrorMessage:"비밀번호가 일치하지않습니다."});
+        return res.status(400).render("user/join",{pageTitle,pwErrorMessage:"비밀번호가 일치하지않습니다."});
     }
     await User.create({
         username,userid:id,password,email,birth
@@ -34,16 +35,17 @@ export const postLogin = async(req,res)=>{
     const {id,password} = req.body;
     const userExist = await User.findOne({userid:id});
     if(!userExist){
-        res.render("user/login",{pageTitle:"Login",idErrorMessage:"존재하지않는 아이디입니다."});
+        return res.status(404).render("user/login",{pageTitle:"Login",idErrorMessage:"존재하지않는 아이디입니다."});
+    }
+
+    if(!(await bcrypt.compare(password,userExist.password))){
+        return res.render("user/login",{pageTitle:"Login",pwErrorMessage:"잘못된 비밀번호입니다."});
     }
     else{
-        console.log(userExist.password,password);
-        if(!(await bcrypt.compare(password,userExist.password))){
-            res.render("user/login",{pageTitle:"Login",pwErrorMessage:"잘못된 비밀번호입니다."});
-        }
-        else{
-            res.redirect("/");
-            // 세션바꾸기s
-        }
+        req.session.loggedIn = true;
+        req.session.user = userExist;
+        return res.redirect("/");
+        
+
     }
 }
--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import "./db.js";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter.js";
 import userRouter from "./routers/userRouter.js";
 import videoRouter from "./routers/videoRouter.js";
+import { localsMiddleware } from "./middleware.js";
 
 const PORT = "4000";
 const app = express();
@@ -21,6 +24,22 @@ const handleOpneServer = (req,res,next) =>{
 
 app.use(logger);
 app.use("/assets",express.static("assets"));
+
+app.use(session({
+    secret:process.env.COOKIE_SECRET,
+    resave:true,
+    saveUninitialized:true,
+    store:MongoStore.create({
+        mongoUrl:process.env.DB_URL
+    })
+}))
+// app.use("/",(req,res,next)=>{
+//     console.log(req.session);
+//     next();
+// })
+
+// 세션과 관련된 코드보다 뒤에 와야 세션값을 받을 수 있음
+app.use(localsMiddleware);
 app.use("/",rootRouter);
 app.use("/users",userRouter);
 app.use("/videos",videoRouter);
